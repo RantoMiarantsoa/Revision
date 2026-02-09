ab248cb58488bf5adf3a9d9c1407f9b1c68a9a58
MiaroYollan <miaroyollan@gmail.com>
2026-02-09 16:45:51 +0300

    model categorie

--- app/model/Categorie.php
+++ app/model/Categorie.php
@@ -4,11 +4,35 @@ namespace app\model;
 
 use Flight;
 class Categorie {
-    public $id;
-    public $nom;
+    private $pdo;
 
-    public function __construct($id, $nom) {
-        $this->id = $id;
-        $this->nom = $nom;
+    public function __construct(\PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function getAll() {
+        $stmt = $this->pdo->query("SELECT * FROM Categorie");
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function getById($id) {
+        $stmt = $this->pdo->prepare("SELECT * FROM Categorie WHERE id_cat = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function create($description) {
+        $stmt = $this->pdo->prepare("INSERT INTO Categorie (description) VALUES (?)");
+        return $stmt->execute([$description]);
+    }
+
+    public function update($id, $description) {
+        $stmt = $this->pdo->prepare("UPDATE Categorie SET description = ? WHERE id_cat = ?");
+        return $stmt->execute([$description, $id]);
+    }
+
+    public function delete($id) {
+        $stmt = $this->pdo->prepare("DELETE FROM Categorie WHERE id_cat = ?");
+        return $stmt->execute([$id]);
     }
 }
