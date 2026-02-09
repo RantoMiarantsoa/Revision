dbe5c7403fdaed8bb632065562b70dde4cf64271
MiaroYollan <miaroyollan@gmail.com>
2026-02-09 16:08:20 +0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -4,12 +4,8 @@
 
 CREATE TABLE IF NOT EXISTS admin (
     id SERIAL PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
     mdp VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE Categorie(
-  id_cat SERIAL PRIMARY KEY,
-  description VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS users(
@@ -18,3 +14,19 @@ CREATE TABLE IF NOT EXISTS users(
   email VARCHAR(50),
   pwd VARCHAR(20)
 );
+
+CREATE TABLE Categorie(
+  id_cat SERIAL PRIMARY KEY,
+  description VARCHAR(50)
+);
+
+CREATE TABLE Objet(
+  id_obj SERIAL PRIMARY KEY,
+  nom VARCHAR(50),
+  description TEXT,
+  id_cat INT,
+  id_user INT,
+  FOREIGN KEY (id_cat) REFERENCES Categorie(id_cat),
+  FOREIGN KEY (id_user) REFERENCES users(id_user)
+);
+
