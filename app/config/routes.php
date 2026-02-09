c5ea0903ca6cc58a45edd51cf27fd52ad4a45c9c
MiaroYollan <miaroyollan@gmail.com>
2026-02-09 15:48:33 +0300

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,13 +1,13 @@
 <?php
-require_once __DIR__ . '/../controllers/AuthController.php';
+require_once __DIR__ . '/../controllers/AdminController.php';
 require_once __DIR__ . '/../services/UserService.php';
 require_once __DIR__ . '/../repositories/UserRepository.php';
 
 // Authentication / home
-Flight::route('GET /', ['AuthController', 'showLogin']);
-Flight::route('POST /login', ['AuthController', 'postLogin']);
-Flight::route('GET /home', ['AuthController', 'showHome']);
-Flight::route('GET /logout', ['AuthController', 'logout']);
+Flight::route('GET /', ['AdminController', 'showLogin']);
+Flight::route('POST /login', ['AdminController', 'postLogin']);
+Flight::route('GET /home', ['AdminController', 'showHome']);
+Flight::route('GET /logout', ['AdminController', 'logout']);
 Flight::route('GET /', function() {
     echo "Bienvenue sur mon application Flight MVC !";
 });
